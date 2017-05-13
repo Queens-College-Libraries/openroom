7376498b45b974d21679be152cab28829d9ec6ad
Kushal Hada (developer) <kushaldeveloper@gmail.com>
2017-05-13 13:16:03 -0400

    remove mysql connect strings and mysql escaping
    Signed-off-by: Kushal Hada (developer) <kushaldeveloper@gmail.com>

--- includes/or-dbinfo.php
+++ includes/or-dbinfo.php
@@ -1,52 +1,21 @@
 <?php
-/*
-*or-dbinfo.php
-*Contains settings and functions for connecting to MySQL.
-*This file should be included in any page that requires database connectivity.
-*This file also sets an array with all values from the settings table.
-*/
-
-//MySQL Database
-/*
-*Information used to connect to MySQL database
-*$dbhost is the host name of the server with MySQL installed
-*$dbuser and $dbpass are the username and password that have
-*	SELECT, INSERT, UPDATE, and DELETE privileges on the openroom database
-*$dbdatabase is the name of the database OpenRoom uses (default: openroom)
-*/
-$dbhost = "localhost";
-$dbuser = "rooms";
-$dbpass = "oZl4eftKZuYx4aTDGcLK2yXrLbePH6iQ3POAcVm2YPtbEMBlCwcTpn2abhpls2v";
-$dbdatabase = "rooms";
-//$dbhost = "localhost";
-//$dbuser = "openroom";
-//$dbpass = "xnNtKs804RoaohzSyUfV6xo7bPtvSJnbL9J7M9SrcWIkv3yaWfw8UhjDqMnfEqG";
-//$dbdatabase = "openroom";
-
-mysql_connect($dbhost, $dbuser, $dbpass) or die('Can\'t connect to the database. Error: ' . mysql_error());
-mysql_select_db($dbdatabase) or die('Can\'t connect to the database. Error: ' . mysql_error());
-require_once(__DIR__ . '/../vendor/autoload.php');
-$settings = [];
-$settings = model\Setting::fetch_all();
 
 foreach ($_POST as $key => $value) {
     if (!is_array($value)) {
-        $_POST[$key] = mysql_real_escape_string($value);
+        $_POST[$key] = $value;
     } else {
         foreach ($value as $key2 => $value2) {
-            $_POST[$key][$key2] = mysql_real_escape_string($value2);
+            $_POST[$key][$key2] = $value2;
         }
     }
 }
 
 foreach ($_GET as $key => $value) {
     if (!is_array($value)) {
-        $_GET[$key] = mysql_real_escape_string($value);
+        $_GET[$key] = $value;
     } else {
         foreach ($value as $key2 => $value2) {
-            $_GET[$key][$key2] = mysql_real_escape_string($value2);
+            $_GET[$key][$key2] = $value2;
         }
     }
 }
-
-?>
