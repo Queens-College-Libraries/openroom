0b12df08c31471b07f6d07e149ad1884bd914ea8
Kushal Hada <kushaldeveloper@gmail.com>
2017-06-27 06:20:44 -0400

    add a connection close
    Signed-off-by: Kushal Hada <kushaldeveloper@gmail.com>

--- ktest.php
+++ ktest.php
@@ -1,17 +1,21 @@
 <?php
 require_once 'vendor/autoload.php';
+
 $newPolicy = 'Rosenthal Library usually has several rooms available to students for group study on a first-come, first-serve basis. These rooms are available to currently registered Queens College students only.
 
 Immediate use of a Group Study Room is made by presenting your valid Queens College ID at the Circulation Desk (located on Level 3 of the Library). If available, a room will be assigned to you for one 2-hour time block. If the room is in use a hold may be placed to secure the next available time slot. Room use, like book use, is assigned to your record in our automated circulation system. When a room is assigned to you, you will be handed a wooden block upon which the room number and policies governing Group Study Rooms is adhered. Upon completing your use of the room, the wooden block is to be returned to the Circulation Desk and the assignment of the room to your record will be released.
 
 Should you wish to extend the use of the room you are required to return to the Circulation desk with your ID and the wooden block at the end of the 2 hours. The room will then be reassigned to you provided there are no other users awaiting use of the room.';
-model\Setting::update(\model\Db::getInstance(), 'policies', $newPolicy);
+$my_connection = \model\Db::getInstance();
+model\Setting::update($my_connection, 'policies', $newPolicy);
 echo preg_replace('/\v+|\\\r\\\n/','<br/>',model\Setting::fetchValue(\model\Db::getInstance(), 'policies'));
 
 $kus = \model\User::create()->setUsername(generateRandomString(16))->setDisplayName(generateRandomString(12))->setPassword(generateRandomString(60))->setEmail(generateRandomString(12).'@' . generateRandomString(6) . '.' . generateRandomString(3));
-model\User::addUser(\model\Db::getInstance(), $kus);
-$users = model\User::fetchAll(\model\Db::getInstance());
+model\User::addUser($my_connection, $kus);
+$users = model\User::fetchAll($my_connection);
 var_dump($users);
+
+$my_connection = null;
 
 function generateRandomString($length = 10) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
