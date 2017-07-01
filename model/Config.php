ba949a5578d477b12ba43fef3d45ae75dc439ebe
Kushal Hada <kushaldeveloper@gmail.com>
2017-07-01 13:49:18 -0400

    add php doc, it is an array in heroku
    Signed-off-by: Kushal Hada <kushaldeveloper@gmail.com>

--- model/Config.php
+++ model/Config.php
@@ -16,10 +16,10 @@ class Config
 
 }
 
-$dbopts = parse_url(getenv('DATABASE_URL'));
-//
-Config::write('db.host', $dbopts["host"]);
-Config::write('db.port', $dbopts["port"]);
-Config::write('db.basename', $dbopts["path"]);
-Config::write('db.user', $dbopts["user"]);
-Config::write('db.password', $dbopts["pass"]);
+/** @var array $db */
+$db = parse_url(getenv('DATABASE_URL'));
+Config::write('db.host', $db["host"]);
+Config::write('db.port', $db["port"]);
+Config::write('db.basename', $db["path"]);
+Config::write('db.user', $db["user"]);
+Config::write('db.password', $db["pass"]);
