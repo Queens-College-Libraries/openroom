e195582035f05a84ef95f5c63db11025d385c8ec
Kushal Hada <kushaldeveloper@gmail.com>
2017-07-25 06:09:57 -0400

    remove redundant ending ?> from reminder.php
    Signed-off-by: Kushal Hada <kushaldeveloper@gmail.com>

--- modules/reminder.php
+++ modules/reminder.php
@@ -3,4 +3,3 @@ if (trim(model\Setting::fetchValue(\model\Db::getInstance(), 'remindermessage'))
     $reminder_string = htmlentities(model\Setting::fetchValue(\model\Db::getInstance(), 'remindermessage'), ENT_COMPAT, 'UTF-8');
     echo "<div id=\"remindermessage\"><span class=\"remindermessage\">" . $reminder_string . "</span></div>";
 }
-?>
