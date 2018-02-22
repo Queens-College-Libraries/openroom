68dcbe1d8bab37ea8229d43aa20a5a80c681ada3
jvangore31 <jvangore31@gmail.com>
2018-02-22 17:29:42 -0600

    Unset variables
    Checked if successmsg and error msg are in use before using

--- editaccount.php
+++ editaccount.php
@@ -52,11 +52,15 @@ if ($_SESSION["systemid"] == $settings["systemid"]) {
 
     <center>
         <?php
-        if ($successmsg != "") {
-            echo "<div id=\"successmsg\">" . $successmsg . "</div>";
+        if(isset($successmsg)) {
+            if ($successmsg != "")  {
+                echo "<div id=\"successmsg\">" . $successmsg . "</div>";
+            }
         }
-        if ($errormsg != "") {
-            echo "<div id=\"errormsg\">" . $errormsg . "</div>";
+        if(isset($errormsg)) {
+            if ($errormsg != "") {
+                echo "<div id=\"errormsg\">" . $errormsg . "</div>";
+            }
         }
         ?>
     </center>
