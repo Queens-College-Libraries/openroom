af90184640777c13328cb26bca5c906d6d0492a2
Kushal Hada <kushaldeveloper@gmail.com>
2018-08-12 05:50:19 -0400

    delete all files to make room for clean deck

--- modules/reminder.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ($settings["remindermessage"] != "") {
-    $reminder_string = htmlentities($settings["remindermessage"], ENT_COMPAT, 'UTF-8');
-    echo "<div id=\"remindermessage\"><span class=\"remindermessage\">" . $reminder_string . "</span></div>";
-}
-?>
