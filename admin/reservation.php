88f0d8f4fd33900142095bb225da3a91c50fbe76
Kushal Hada <kushaldeveloper@gmail.com>
2018-08-16 20:32:23 -0400

    fix permission issues with new reservation page

--- admin/reservation.php
+++ admin/reservation.php
@@ -1,10 +1,13 @@
 <?php
 require_once "../vendor/autoload.php";
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 $loader = new Twig_Loader_Filesystem(__DIR__ . "/../themes/kushal/");
 $twig = new Twig_Environment($loader, array('debug' => true));
 $twig->addExtension(new Twig_Extension_Debug());
 
-if (!(isset($_SESSION["username"])) || $_SESSION["username"] == "") {
+if (!isset($_SESSION["username"]) || $_SESSION["username"] == "") {
     try {
         $template = $twig->load('notLoggedIn.html');
     } catch (Twig_Error_Loader $e) {
@@ -28,8 +31,18 @@ if (!(isset($_SESSION["username"])) || $_SESSION["username"] == "") {
     }
     echo $template->render(array('title' => \model\Setting::find("instance_name")->get_value(),
         'page_title' => "Not authorized"));
-} elseif ($_SESSION["systemid"] != $settings["systemid"]) {
-    echo "You are not logged in. Please <a href=\"../index.php\">click here</a> and login with an account that is an authorized administrator or reporter.";
+} elseif (isset($_SESSION["systemid"]) && $_SESSION["systemid"] != \model\Setting::find("systemid")->get_value()) {
+    try {
+        $template = $twig->load('notLoggedIn.html');
+    } catch (Twig_Error_Loader $e) {
+        var_dump($e);
+    } catch (Twig_Error_Runtime $e) {
+        var_dump($e);
+    } catch (Twig_Error_Syntax $e) {
+        var_dump($e);
+    }
+    echo $template->render(array('title' => \model\Setting::find("instance_name")->get_value(),
+        'page_title' => "You need to log in separately for each application."));
 } else {
     try {
         $template = $twig->load('reservation.html');
