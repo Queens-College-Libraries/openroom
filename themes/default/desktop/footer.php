2a651dea6aca2b284bdf23345189504e005cb984
Kushal <kushaldeveloper@gmail.com>
2019-01-26 08:08:35 -0500

    show current server time in footer

--- themes/default/desktop/footer.php
+++ themes/default/desktop/footer.php
@@ -4,6 +4,9 @@
 <br/>
 Developed by Ball State University Libraries<br/>
 Copyright (C) 2012 Ball State University Libraries
+<?php 
+echo "Server time: " .  date_format($today,"Y-m-d H:i:s");
+?>
 </div>
 <div id="themeswitch" onClick="javascript:document.cookie='theme=mobile';location.reload(true);">Switch to Mobile View
 </div>
