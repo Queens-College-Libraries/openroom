3c4ee470c671d29f75faff2bcea22626c2700fbf
Kushal <kushaldeveloper@gmail.com>
2019-01-26 20:11:49 -0500

    remove commented code
    fixes https://github.com/openroom/openroom/issues/41
    https://github.com/openroom/openroom/issues/42
    https://github.com/openroom/openroom/issues/42
    https://github.com/openroom/openroom/issues/43
    https://github.com/openroom/openroom/issues/44
    https://github.com/openroom/openroom/issues/45
    https://github.com/openroom/openroom/issues/46
    https://github.com/openroom/openroom/issues/47

--- model/Db.php
+++ model/Db.php
@@ -1,38 +1,6 @@
 <?php
 declare(strict_types=1);
 namespace model;
-// class Db
-// {
-//     public $dbh; // handle of the db connexion
-//     private static $instance;
-
-//     private function __construct()
-//     {
-//         // building data source name from config
-//         $dsn = 'pgsql:host=' . Config::read('db.host') .
-//                ';dbname='    . Config::read('db.basename') .
-//                ';port='      . Config::read('db.port') .
-//                ';connect_timeout=15';
-//         // getting DB user from config                
-//         $user = Config::read('db.user');
-//         // getting DB password from config                
-//         $password = Config::read('db.password');
-
-//         $this->dbh = new PDO($dsn, $user, $password);
-//     }
-
-//     public static function getInstance()
-//     {
-//         if (!isset(self::$instance))
-//         {
-//             $object = __CLASS__;
-//             self::$instance = new $object;
-//         }
-//         return self::$instance;
-//     }
-
-//     // others global functions
-// }
 class Db
 {
     private static $instance = NULL;
